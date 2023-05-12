b5366aa7129a4971ddfb24cf2a866b94f0e730fd
incha111 <87800456+incha111@users.noreply.github.com>
2023-05-12 23:21:24 +0200

    Update Person.java
    add field nickName

--- demo/src/main/java/com/incha/demo/domain/Person.java
+++ demo/src/main/java/com/incha/demo/domain/Person.java
@@ -3,6 +3,7 @@ package com.incha.demo.domain;
 public class Person {
     private String name;
     private String age;
+    private String nickName;
     private String email;
 
     public String getName() {
@@ -13,8 +14,9 @@ public class Person {
         return age;
     }
 
-    public Person(String name, String age) {
+    public Person(String name, String age, String nickName) {
         this.name = name;
         this.age = age;
+        this.nickName = nickName;
     }
 }
